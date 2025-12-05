5aaf38a5c5ea3b9ca7b21f0892943f6741c8673b
Sahil Chouhan <chouhangolu604@gmail.com>
2025-12-05 11:52:27 +0530

    update

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/addtask.css'
 const AddTask = () => {
+  const [taskData, setTaskData] = useState({})
+  const handleAddTask = async ()=>{
+    console.log(taskData)
+    let result = await fetch("http://localhost:3200/add-task", {
+      method: "Post",
+      body:JSON.stringify(taskData),
+      headers:{
+        'Content-Type': 'Application/Json'
+      }
+    
+    })
+    result = await result.json()
+    if(result){
+      console.log('new task added')
+    }
+  }
   return (
     <div className='container'>
       <h1>Add New Task</h1>
-      <form action="">
+      
         <label htmlFor="">Title</label>
-        <input type="text" name="title" placeholder='Enter Task Title' />
+        <input onChange={(event)=>setTaskData({...taskData, title:event.target.value})} type="text" name="title" placeholder='Enter Task Title' />
         <label htmlFor="">Description</label>
-        <textarea rows={4} name="description" placeholder='Enter Task Description'></textarea>
-        <button className='submit'>Add New Task</button>
-      </form>
+        <textarea onChange={(event)=>setTaskData({...taskData, description:event.target.value})} rows={4} name="description" placeholder='Enter Task Description'></textarea>
+        <button onClick={handleAddTask} className='submit'>Add New Task</button>
+     
     </div>
   )
 }
